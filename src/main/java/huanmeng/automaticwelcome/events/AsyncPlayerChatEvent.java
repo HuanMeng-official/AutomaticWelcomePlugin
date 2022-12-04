ba55e37915f0da1150b2a00817721c862e1e9724
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2022-12-04 18:59:55 +0800

    Add files via upload

--- src/main/java/huanmeng/automaticwelcome/events/AsyncPlayerChatEvent.java
+++ src/main/java/huanmeng/automaticwelcome/events/AsyncPlayerChatEvent.java
@@ -10,36 +10,53 @@ public class AsyncPlayerChatEvent implements Listener {
     Plugin plugin = huanmeng.automaticwelcome.AutomaticWelcome.getPlugin(huanmeng.automaticwelcome.AutomaticWelcome.class);
     @EventHandler
     public void AsyncPlayerChatEvent(org.bukkit.event.player.AsyncPlayerChatEvent say){
-        if (say.getMessage().contains("sb"));
-        Player chatsay = say.getPlayer();
-        chatsay.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
-
-        if (say.getMessage().contains("傻逼"));
-        Player chatsay1 = say.getPlayer();
-        chatsay1.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
-
-        if (say.getMessage().contains("几把"));
-        Player chatsay2 = say.getPlayer();
-        chatsay2.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
-
-        if (say.getMessage().contains("他妈"));
-        Player chatsay3 = say.getPlayer();
-        chatsay3.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
-
-        if (say.getMessage().contains("你妈死了"));
-        Player chatsay4 = say.getPlayer();
-        chatsay4.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
-
-        if (say.getMessage().contains("操你妈"));
-        Player chatsay5 = say.getPlayer();
-        chatsay5.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
-
-        if (say.getMessage().contains("脑瘫"));
-        Player chatsay6 = say.getPlayer();
-        chatsay6.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
-
-        if (say.getMessage().contains("fuck"));
-        Player chatsay7 = say.getPlayer();
-        chatsay7.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
+        if (say.getMessage().contains("sb")) {
+            Player chatsay = say.getPlayer();
+            chatsay.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
+        }
+        if (say.getMessage().contains("傻逼")) {
+            Player chatsay1 = say.getPlayer();
+            chatsay1.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
+        }
+        if (say.getMessage().contains("几把")) {
+            Player chatsay2 = say.getPlayer();
+            chatsay2.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
+        }
+        if (say.getMessage().contains("他妈")) {
+            Player chatsay3 = say.getPlayer();
+            chatsay3.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
+        }
+        if (say.getMessage().contains("你妈死了")) {
+            Player chatsay4 = say.getPlayer();
+            chatsay4.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
+        }
+        if (say.getMessage().contains("操你妈")) {
+            Player chatsay5 = say.getPlayer();
+            chatsay5.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
+        }
+        if (say.getMessage().contains("脑瘫")) {
+            Player chatsay6 = say.getPlayer();
+            chatsay6.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
+        }
+        if (say.getMessage().contains("fuck")) {
+            Player chatsay7 = say.getPlayer();
+            chatsay7.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
+        }
+        if (say.getMessage().contains("神经病")){
+            Player chatsay8 = say.getPlayer();
+            chatsay8.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
+        }
+        if (say.getMessage().contains("屎")){
+            Player chatsay9 = say.getPlayer();
+            chatsay9.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
+        }
+        if (say.getMessage().contains("你妈")){
+            Player chatsay10 = say.getPlayer();
+            chatsay10.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.RED + plugin.getConfig().getString("Alert"));
+        }
+        if (say.getMessage().contains("ban")){
+            Player chatsay11 = say.getPlayer();
+            chatsay11.kickPlayer(ChatColor.RED + plugin.getConfig().getString("Kick"));
+        }
     }
 }
