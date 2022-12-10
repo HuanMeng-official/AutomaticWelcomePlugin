363439eb163dfd6247979647bb219fbbcc4eebc0
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2022-12-10 16:15:26 +0800

    Add files via upload

---
+++ src/main/java/huanmeng/automaticwelcome/commands/SetSpectator.java
@@ -0,0 +1,20 @@
+package huanmeng.automaticwelcome.commands;
+
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SetSpectator implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player){
+            Player player = (Player) sender;
+            ((Player) sender).setGameMode(GameMode.SPECTATOR);
+        }else{
+            System.out.println("Error: Please let the player execute the command");
+        }
+        return false;
+    }
+}
