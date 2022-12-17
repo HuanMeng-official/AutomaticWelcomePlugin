0baf974d8dfc81c60a2870abc03ce43a531b5cf8
HuanMeng <110048961+HuanMeng-official@users.noreply.github.com>
2022-12-17 17:48:57 +0800

    Add files via upload

--- src/main/java/huanmeng/automaticwelcome/says/StemWords.java
+++ src/main/java/huanmeng/automaticwelcome/says/StemWords.java
@@ -27,24 +27,29 @@ public class StemWords implements Listener {
             stem2.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Stemalert"));
         }
         if (say.getMessage().contains("宇将军")){
-            Player stem2 = say.getPlayer();
+            Player stem3 = say.getPlayer();
             say.setCancelled(plugin.getConfig().getBoolean("Stem"));
-            stem2.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Stemalert"));
+            stem3.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Stemalert"));
         }
         if (say.getMessage().contains("半只狐狸")){
-            Player stem2 = say.getPlayer();
+            Player stem4 = say.getPlayer();
             say.setCancelled(plugin.getConfig().getBoolean("Stem"));
-            stem2.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Stemalert"));
+            stem4.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Stemalert"));
         }
         if (say.getMessage().contains("沈阳大街")){
-            Player stem2 = say.getPlayer();
+            Player stem5 = say.getPlayer();
             say.setCancelled(plugin.getConfig().getBoolean("Stem"));
-            stem2.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Stemalert"));
+            stem5.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Stemalert"));
         }
         if (say.getMessage().contains("jvav")){
-            Player stem2 = say.getPlayer();
+            Player stem6 = say.getPlayer();
             say.setCancelled(plugin.getConfig().getBoolean("Stem"));
-            stem2.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Stemalert"));
+            stem6.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Stemalert"));
+        }
+        if (say.getMessage().contains("jave")){
+            Player stem7 = say.getPlayer();
+            say.setCancelled(plugin.getConfig().getBoolean("Stem"));
+            stem7.sendMessage(ChatColor.YELLOW +plugin.getConfig().getString("Characters") + ChatColor.WHITE + plugin.getConfig().getString("Stemalert"));
         }
     }
 }
